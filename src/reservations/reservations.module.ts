633a3dcaed761c8a97c6f456ec112988bad2813e
Guilherme Kuhn <guilherme.kuhn002@edu.pucrs.br>
2025-11-17 19:47:39 -0300

    fea: updated reservations BFF

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 import { HttpClientModule } from '../common/services/http-client.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, UsersModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
