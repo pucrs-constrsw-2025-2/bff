80c330a18934b82e051454a0735a165583926df4
Guilherme Kuhn <guilherme.kuhn002@edu.pucrs.br>
2025-11-17 20:23:33 -0300

    feature: finished reservations

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
 import { HttpClientModule } from '../common/services/http-client.module';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [HttpClientModule, UsersModule],
+  imports: [HttpClientModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
